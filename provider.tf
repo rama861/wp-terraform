aad6ef3775e4543ec3815972ab50e0a6c65a9b0c
mujahidshaik28 <116384473+mujahidshaik28@users.noreply.github.com>
2024-02-14 15:34:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region                  = "us-west-2"
-  access_key              = var.AWS_ACCESS_KEY_ID
-  secret_key              = var.AWS_SECRET_ACCESS_KEY
+  region = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
