5173360f9144fdd3f1b900d588929d2e5f3acaab
rama861 <161824892+rama861@users.noreply.github.com>
2024-03-28 12:37:14 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating EC2 Instance
 resource "aws_instance" "wordpress8" {
-  ami                         = "ami-000c0df09737657b6"
+  ami                         = "ami-033a1ebf088e56e81"
   instance_type               = "t2.micro"
-  key_name                    = "p4-key"
+  key_name                    = "ram"
   vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
